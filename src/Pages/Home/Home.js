db5cdcd26f061bca443775b9fe9db90eadeb9350
sainath-kadam <mydearsainathkadam@gmail.com>
2023-07-21 00:14:19 +0530

    HP Animation1 added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,16 +4,23 @@ import SocialMedia from '../../Components/SocialMedia/SocialMedia';
 import NameAndTagline from '../../Components/NameAndTagline/NameAndTagline';
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
+import Animation1 from '../../Components/Animation1/Animation'
 
 function Home() {
 
   return (   
+    <>
     <div className='home'>
-      <Navbar/>
+      {/* <Navbar/> */}
+      
       <SocialMedia className='social-media'/>
       <NameAndTagline className="name-and-tagline"/>
-      <Footer className='footer'/>
+      {/* <Footer className='footer'/> */}
     </div>
+    <div>
+    <Animation1 className="animation1"/>
+    </div>
+    </>
   )
 }
 
