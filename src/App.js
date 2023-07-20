cb2b81a7face3a38f7d0bf0e4c54ca9cfca04beb
sainath-kadam <mydearsainathkadam@gmail.com>
2023-07-20 23:31:30 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-=======
-// import './App.css';
-// import './WaterDroplet.css';
-// import React, { useState, useEffect } from 'react';
-import { useContext } from 'react';
-import { Route, Routes } from "react-router-dom";
->>>>>>> 4686bbbc377fe23a02f83d0c0cd76d8c0c17056a
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Associates from './Pages/Associates/Associates';
@@ -15,19 +7,11 @@ import Gallery from './Pages/Gallery/Gallery';
 import Team from './Pages/Team/Team';
 import Contact from './Pages/Contact/Contact';
 import Events from './Pages/Events/Events';
-<<<<<<< HEAD
 import Footer from './Components/Footer/Footer';
-=======
-import Gallery from './Pages/Gallery/Gallery';
-import Associates from './Pages/Associates/Associates';
-import Team from './Pages/Team/Team';
-
->>>>>>> 4686bbbc377fe23a02f83d0c0cd76d8c0c17056a
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Router>
         <>
           <Navbar />
@@ -43,55 +27,6 @@ function App() {
           <Footer />
         </>
       </Router>
-=======
-
-      {/* <Home>
-
-      <Routes>
-        <Route path="/"
-          element={
-            <Home
-            />
-          }
-        />
-        <Route
-          path="/about"
-          element={<About />}
-        />
-        <Route
-          path="/events"
-          element={<Events />}
-        />
-        <Route
-          path="/contacts"
-          element={
-            <Contact />
-          }
-        />
-        <Route
-          path="/gallery"
-          element={
-            <Gallery />
-          }
-        />
-        <Route path="/team"
-          element={<Team />} />
-
-        <Route
-          path="/associates"
-          element={<Associates />}
-        />
-      </Routes>
-
-      </Home> */}
-
-      {/* <Navbar/>  */}
-      {/* <About /> */}
-      <Home />
-      {/* <Contact/> */}
-      {/* <Events/> */}
-
->>>>>>> 4686bbbc377fe23a02f83d0c0cd76d8c0c17056a
     </div>
   );
 }
