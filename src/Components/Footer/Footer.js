98e3ca8a3e3021233f1aeb0d3a9440741d1b1b5f
Suman raj <raj_suman@hotmail.com>
2023-07-20 12:11:46 +0530

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>
-      
+    <div className='footer'>
+        <div className='copyrights'>All Rights Reserved &copy;E-Cell IIIT Tiruchirappalli</div>
+        <div className='developer'>Made with 	&hearts; By Web Team E-cell IIITT</div>
     </div>
   )
 }
 
 export default Footer
+
+{/* <span class="word-gap"></span> */}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,47 +1,18 @@
 import React from 'react';
-// import { IconButton, colors, withTheme } from '@mui/material';
-// import InstagramTcon from '@mui/icons-material/Instagram';
-// import TwitterIcon from '@mui/icons-material/Twitter';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-// import FacebookIcon from '@mui/icons-material/Facebook';
-// import MailOutlineIcon from '@mui/icons-material/MailOutline';
-
 import './Home.css'
 import SocialMedia from '../../Components/SocialMedia/SocialMedia';
 import NameAndTagline from '../../Components/NameAndTagline/NameAndTagline';
-// import BackgroungAnimation from '../BackgroundAnimation/BackgroundAnimation'
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
 
 function Home() {
 
-//   const white = {
-//     color:'grey'
-// }
-
-    
-    // const tagline = '"Catalyzing Reformers"';
-    // const [animatedText, setAnimatedText] = useState("");
-    // // const tagline = "Catalyzing Reformers";
-    // let i = 1;
-    // console.log(tagline.length);
-  
-    // useEffect(() => {
-    //   const typingInterval = setInterval(() => {
-    //     if (i < tagline.length) {
-    //       setAnimatedText((prevText) => prevText + tagline.charAt(i));
-    //       console.log(tagline.charAt(i));
-    //       i++;
-    //     } else {
-    //       clearInterval(typingInterval);
-    //     }
-    //   }, 100); // Adjust the interval for the typing speed
-  
-    //   return () => clearInterval(typingInterval);
-    // }, []);
-
   return (   
     <div className='home'>
+      <Navbar/>
       <SocialMedia className='social-media'/>
       <NameAndTagline className="name-and-tagline"/>
+      <Footer className='footer'/>
     </div>
   )
 }
