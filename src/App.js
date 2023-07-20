e6328630cc380b3b532d9c5ccde4f4d7fe4e62d7
sainath-kadam <mydearsainathkadam@gmail.com>
2023-07-20 23:26:09 +0530

    Routing Done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,97 @@
-// import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
-import Navbar from './Components/Navbar/Navbar';
+import Associates from './Pages/Associates/Associates';
+import Gallery from './Pages/Gallery/Gallery';
+import Team from './Pages/Team/Team';
 import Contact from './Pages/Contact/Contact';
 import Events from './Pages/Events/Events';
+import Footer from './Components/Footer/Footer';
 
 function App() {
-
-  // const aboutSection = useRef(null);
-  // const organizerSection = useRef(null);
-
-  // const gotoAbout = () =>
-  //   window.scrollTo({ top: aboutSection.current.offsetTop });
-  // const gotoOrganizer = () =>
-  //   window.scrollTo({ top: organizerSection.current.offsetTop });
-
   return (
     <div className="App">
-
-      {/* <Navbar/>  */}
-      {/* <About /> */}
-      <Home /> 
-      {/* <Contact/> */}
-      {/* <Events/> */}
-
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Associates" element={<Associates />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Team" element={<Team />} />
+          </Routes>
+          <Footer />
+        </>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// import { BrowserRouter as Router, Route, useRoutes } from 'react-router-dom';
+ 
+// import Navbar from './Components/Navbar/Navbar';
+// import Home from './Pages/Home/Home';
+// import About from './Pages/About/About';
+// import Associates from './Pages/Associates/Associates';
+// import Gallery from './Pages/Gallery/Gallery';
+// import Team from './Pages/Team/Team';
+// import Contact from './Pages/Contact/Contact';
+// import Events from './Pages/Events/Events';
+// import Footer from './Components/Footer/Footer';
+
+// function App() {
+
+//   // const aboutSection = useRef(null);
+//   // const organizerSection = useRef(null);
+
+//   // const gotoAbout = () =>
+//   //   window.scrollTo({ top: aboutSection.current.offsetTop });
+//   // const gotoOrganizer = () =>
+//   //   window.scrollTo({ top: organizerSection.current.offsetTop });
+
+//   return (
+//     <div className="App">
+
+
+// <>
+//             <Navbar />
+//             <Routes>
+//               <Route path="/" element={<Home />} />
+//               <Route path="/About" element={<About />} />
+//               <Route path="/Contact" element={<Contact />} />
+//               <Route path="/Associates" element={<Associates />} />
+//               <Route path="/Events" element={<Events />} />
+//               <Route path="/Gallery" element={<Gallery />} />
+//               <Route path="/Team" element={<Team />} />
+//             </Routes>
+//             <Footer/>
+//           </>
+
+      {/* <Navbar/>  */}
+      {/* <About /> */}
+      {/* <Home />  */}
+      {/* <Contact/> */}
+      {/* <Events/> */}
+
+    {/* </div>
+  );
+}
+
+export default App; */}
 
 // Events route below
       // <Route
